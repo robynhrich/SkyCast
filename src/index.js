a0b62845bd6a9d13e471adfe7b713290ab6f780d
Robyn H Richardson <robynhrich@icloud.com>
2023-11-30 12:05:10 -0500

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#weather-app-temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+
+  tempElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  //make the api call and update to interface
+  let apiKey = "1o334f343ac7e4f31b2ae638t364b007";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
-  //call the API
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Miami");
