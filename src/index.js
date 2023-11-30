7aca4fe8b3eb5973a3c2bde94cb6da7e84f73287
Robyn H Richardson <robynhrich@icloud.com>
2023-11-30 12:53:32 -0500

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,38 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = response.data.city;
 
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = ` ${Math.round(
+    response.data.wind.speed / 1.609344
+  )} mph`;
+  let dateElement = document.querySelector("#weather-app-date");
+  let date = new Date(response.data.time * 1000);
+
+  dateElement.innerHTML = formateDate(date);
+
+  function formateDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+  }
+
   tempElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
